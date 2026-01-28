b23406392360cc69ff65737b46cc66897732ccb5
Timothy <timothysuldayan1@gmail.com>
2026-01-27 17:04:10 -0800

    configured cli commands and runner

---
+++ orchestrator/src/main/java/com/example/orchestrator/cli/Command.java
@@ -0,0 +1,29 @@
+package com.example.orchestrator.cli;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class Command implements CommandLineRunner {
+    private static final short MAX_NUM_ARGS = 2;
+
+    @Override
+    public void run(String... args) {
+        try {
+            if (args.length != MAX_NUM_ARGS) {
+                throw new IllegalArgumentException("Invalid number of arguments.\nUsage: <path to ohlcv.csv> <path to strategy config>");
+            }
+
+            final String OHLCV_PATH = args[0];
+            final String CONFIG_PATH = args[1];
+
+            log.info("Running backtest...");
+            log.info("OHLCV Path: {} \n Strategy Config Path: {}", OHLCV_PATH, CONFIG_PATH);
+        } catch (Exception e) {
+            log.error("Backtest failed: {}", e.getMessage());
+            System.exit(1);
+        }
+    }
+}
