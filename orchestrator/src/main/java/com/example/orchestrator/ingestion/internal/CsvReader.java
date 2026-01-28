bcdfa46792428a125be7907314e9c5989c0e8ce6
Timothy <timothysuldayan1@gmail.com>
2026-01-27 17:19:55 -0800

    enabled preview features

---
+++ orchestrator/src/main/java/com/example/orchestrator/ingestion/internal/CsvReader.java
@@ -0,0 +1,13 @@
+package com.example.orchestrator.ingestion;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public final class CsvReader implements IngestionService {
+    @Override
+    public void extract() {
+
+    }
+}
